978d30a54da299b098ce8dde203ffea39f404671
Kaylal12 <kaylalei224@gmail.com>
2015-10-12 23:48:14 -0400

    Added jQuery code for gameplay (X, O)

--- application.js
+++ application.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  var turn = 1;
+  var game = true;
+
+  $(".square").click(function() {
+    if ($(this).text()=="" && game) {
+      if (turn % 2 == 1) {
+        $(this).append("X");
+      }
+      else {
+        $(this).append("O");
+      }
+
+      turn++;
+    }
+  })
 
 });
 
